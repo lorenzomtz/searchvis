4d01baa3e87f9ab3ef69aa19d07d4bee90ffc212
Lorenzo Martinez <lorenzomtz@utexas.edu>
2021-05-21 15:39:38 -0500

    initialized square class

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from queue import PriorityQueue
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 
+# color constants
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -16,9 +17,46 @@ ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
-def main():
-    print("oh shit, u got it to run")
-    pygame.display.set_caption("Path Finding Algorithms")
+# a square on the grid
+class Square:
 
-if __name__ == "__main__":
-    main()
+    # initialize a square
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_neighbors(self):
+        return self.neighbors
+
+    def get_row(self):
+        return self.row
+    
+    def get_col(self):
+        return self.col
+
+    def get_color(self):
+        return self.color
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+    
+    def get_width(self):
+        return self.width
+
+    def get_total_rows(self):
+        return self.total_rows
+
+    def main():
+        pygame.display.set_caption("Path Finding Algorithms")
+
+    if __name__ == "__main__":
+        main()
