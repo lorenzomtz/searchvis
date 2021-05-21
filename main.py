44a791d12d757bc04f580b5f3be3912a3ddc9de3
Lorenzo Martinez <lorenzomtz@utexas.edu>
2021-05-21 15:25:32 -0500

    basic display, set color constants

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
 def main():
     print("oh shit, u got it to run")
+    pygame.display.set_caption("Path Finding Algorithms")
 
 if __name__ == "__main__":
     main()
