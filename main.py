31a7d9e668ab30e9c7f9ae3971b9572d56232be8
Lorenzo Martinez <lorenzomtz@utexas.edu>
2021-06-02 21:56:27 -0500

    refactored code throughout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
-import math
 import search
 from colour import Color
+
 
 # key constants from pygame
 from pygame.locals import (
@@ -13,69 +13,82 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+search_actions = [K_UP, K_DOWN, K_LEFT, K_RIGHT]
 
-legal_actions = [K_UP, K_DOWN, K_LEFT, K_RIGHT]
 
 # color constants
-# TODO: find prettier colors
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
+SCALE = 255
 
 
-# set up square screen
+# screen/grid constants
 WIDTH = 800
 MARGIN = 1
 SQ_WIDTH = 20
 GRID_LENGTH = 38
-SCALE = 255
-pg.init()  
+
+
+# set up square screen/grid
+pg.init()
 screen = pg.display.set_mode((WIDTH, WIDTH))
 clock = pg.time.Clock()
 grid = []
-rects = []
 start = (3, 5)
 end = (10, 29)
+
 
 # a square on the grid
 class Square:
 
+
     # initialize a square
-    def __init__(self, row, col, width, total_rows):
+    def __init__(self, row, col):
         self.row = row
         self.col = col
         self.color = WHITE
         self.neighbors = []
-        self.width = width
-        self.total_rows = total_rows
 
+
+    # getter for neighbor list
     def get_neighbors(self):
         return self.neighbors
 
+
+    # getter for position
     def get_pos(self):
         return self.row, self.col
 
+
+    # getter for color
     def get_color(self):
         return self.color
 
+
+    # setter for color
     def set_color(self, color):
         self.color = color
 
+
+    # setter for neighbor list
     def set_neighbors(self, neighbors):
         self.neighbors = neighbors
 
+
+    # string representation of Square object:
+    # Square at: (y, x)
     def __str__(self):
         return "\nSquare at: (" + str(self.row) + ", " + str(self.col) + ")"
 
+
+    # string representation of Square object when iterating
     def __repr__(self):
         return "\nSquare at: (" + str(self.row) + ", " + str(self.col) + ")"
+
 
 
 # basic screen management
@@ -88,21 +101,21 @@ def setup_screen():
 # setup grid of Square objects
 def setup_grid():
     for y in range(GRID_LENGTH):
-        grid.append([])
-        rects.append([])
+        grid.append([])  
         for x in range(GRID_LENGTH):
-            square = Square(y, x, SQ_WIDTH, GRID_LENGTH)
+            square = Square(y, x)
             grid[y].append(square)
             color = WHITE
+            
             # change start colors
             if y == start[0] and x == start[1] \
                 or y == end[0] and x == end[1]:
                 color = GREEN if x == start[1] and y == start[0] else RED
+            
             grid[y][x].set_color(color)
             rect = pg.draw.rect(screen, color, \
                 [(MARGIN + SQ_WIDTH) * x + MARGIN, \
                     (MARGIN + SQ_WIDTH) * y + MARGIN, SQ_WIDTH, SQ_WIDTH])
-            rects[y].append(rect)
 
     pg.display.flip()
 
@@ -113,10 +126,12 @@ def populate_neighbors():
     for y in range(GRID_LENGTH):
         for x in range(GRID_LENGTH):
             neighbors = []
+            
             if x+1 in range(GRID_LENGTH): neighbors.append((grid[y][x+1], 1))
             if x-1 in range(GRID_LENGTH): neighbors.append((grid[y][x-1], 1))
             if y-1 in range(GRID_LENGTH): neighbors.append((grid[y-1][x], 1))
             if y+1 in range(GRID_LENGTH): neighbors.append((grid[y+1][x], 1))
+            
             grid[y][x].set_neighbors(neighbors)
 
 
@@ -133,6 +148,7 @@ def make_wall(x, y):
 def display_path(squares):
     for sq in squares:
         color = sq.get_color()
+        # recolor squares chosen as path squares
         if color != GREEN and color != RED and color != GREY:
             y, x = sq.get_pos()
             color = BLACK
@@ -141,11 +157,12 @@ def display_path(squares):
             pg.time.delay(5)
 
 
-# erase any path (BLACK) squares displayed on the grid
+# erase any path squares displayed on the grid
 def clear_path():
     for y in range(GRID_LENGTH):
         for x in range(GRID_LENGTH):
             color = grid[y][x].get_color()
+            # override squares that aren't start, end, or wall squares
             if color != GREEN and color != RED and color != GREY:
                 color = WHITE
                 grid[y][x].set_color(color)
@@ -163,28 +180,45 @@ def draw_square(color, x, y):
 # draws the squares at the coordinates given on the screen
 # with a color transition from beginning to end
 def draw_squares_at(coords):
+    # length of coordinate list for display purposes
     total = len(coords)
     green = Color("green")
     red = Color("red")
+
     # creates a range of colors to use for display
     colors = list(green.range_to(red, total))
     count = 0
+
     # loop through all of the coordinates given
     for y, x in coords:
         square = grid[y][x]
         sq_color = square.get_color()
+        
         # avoid coloring over start and end nodes
         if sq_color != RED and sq_color != GREEN and sq_color != GREY:
             # get the rgb values for the current square
             # and convert them to be in the 255 range
-            rgb = list(colors[count].rgb)
-            scaled_color = tuple([c * SCALE for c in rgb])
-            rect = pg.draw.rect(screen, scaled_color, \
+            raw_rgb = list(colors[count].rgb)
+            adjusted_rgb = tuple([c_val * SCALE for c_val in raw_rgb])
+            rect = pg.draw.rect(screen, adjusted_rgb, \
                 [(MARGIN + SQ_WIDTH) * x + MARGIN, \
                     (MARGIN + SQ_WIDTH) * y + MARGIN, SQ_WIDTH, SQ_WIDTH])
             pg.display.update(rect)
+        
         count += 1
         pg.time.delay(5)
+
+
+# helper function for transferring mouse coordinates
+# to grid coordinates
+def get_coords():
+    pos = pg.mouse.get_pos()
+                    
+    # Change the x/y screen coordinates to grid coordinates
+    x = pos[0] // (SQ_WIDTH + MARGIN)
+    y = pos[1] // (SQ_WIDTH + MARGIN)
+
+    return x, y
 
 
 # helper function to perform the specified type
@@ -194,6 +228,7 @@ def search_handler(type):
     clear_path()
     y = start[0]
     x = start[1]
+    
     # up arrow key: BFS
     if type == pg.K_UP:
         path, squares = search.bfs(grid[y][x])
@@ -206,7 +241,7 @@ def search_handler(type):
     # right arrow key: A*
     elif type == pg.K_RIGHT:
         path, squares = search.astar(grid[y][x], end)
-
+    
     # display pathfinding process and resulting path found
     draw_squares_at(squares)
     display_path(path)
@@ -226,77 +261,96 @@ def main():
     rect_x = None
     rect_y = None
     rect_color = None
+    
     # game loop
     while running:
+        # loop through event queue
         for event in pg.event.get():
             global start
             global end
+            
+            # key pressed
             if event.type == KEYDOWN:
                 # escape key: EXIT
                 if event.key == K_ESCAPE:
                     running = False
                     pg.quit()
                 # arrow keys: SEARCH
-                elif event.key in legal_actions:
+                elif event.key in search_actions:
                     search_handler(event.key)
+            
             # window close button: EXIT
             elif event.type == QUIT:
                 running = False
                 pg.quit()
+            
             # mouse moving
             elif event.type == pg.MOUSEMOTION:
                 # left click held while moving
                 if pg.mouse.get_pressed()[0]:
-                    pos = pg.mouse.get_pos()
-                    # Change the x/y screen coordinates to grid coordinates
-                    x = pos[0] // (SQ_WIDTH + MARGIN)
-                    y = pos[1] // (SQ_WIDTH + MARGIN)
+                    # translate mouse coordinates to grid coordinates
+                    x, y = get_coords()
+                    
                     # Set that location to GREY
                     color = grid[y][x].get_color()
+                    
                     # if not start or ending square or being dragged, make wall
                     if color != GREEN and color != RED and not drag:
                         make_wall(x, y)
+            
+            # mouse clicked
             elif event.type == pg.MOUSEBUTTONDOWN:
                 # left click
                 if event.button == 1:
-                    pos = pg.mouse.get_pos()
-                    # Change the x/y screen coordinates to grid coordinates
-                    x = pos[0] // (SQ_WIDTH + MARGIN)
-                    y = pos[1] // (SQ_WIDTH + MARGIN)
+                    # translate mouse coordinates to grid coordinates
+                    x, y = get_coords()
+                    
                     # Set that location to GREY
                     color = grid[y][x].get_color()
-                    # click and drag start or ending square to move
+                    
+                    # click and drag start or ending squa
+                    # re to move
                     if color == GREEN or color == RED:
                         drag = True
                         rect_x = x
                         rect_y = y
                         rect_color = color
+
+                    # otherwise, make wall
+                    else:
+                        make_wall(x, y)
+                
                 # right click, clear grid
                 elif event.button == 3:
                     setup()
+            
+            # mouse button released
             elif event.type == pg.MOUSEBUTTONUP:
                 pressed = False
+                
                 # handle dragging start and end squares
                 if drag:
-                    pos = pg.mouse.get_pos()
-                    # Change the x/y screen coordinates to grid coordinates
-                    x = pos[0] // (SQ_WIDTH + MARGIN)
-                    y = pos[1] // (SQ_WIDTH + MARGIN)
+                    # translate mouse coordinates to grid coordinates
+                    x, y = get_coords()
+                    
                     # update colors of dragged start and endpoint squares
                     draw_square(WHITE, rect_x, rect_y)
                     draw_square(rect_color, x, y)
                     grid[rect_y][rect_x].set_color(WHITE)
                     grid[y][x].set_color(rect_color)
+                    
                     # set new start or end coords
                     if rect_color == GREEN:
                         start = (y, x)
                     elif rect_color == RED:
                         end = (y, x)
+                    
                     # clear dragging information
                     rect_x = None
                     rect_y = None
                     rect_color = None
                     drag = False
+
 
 if __name__ == "__main__":
     main()
